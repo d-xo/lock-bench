bff740ba7b083ac7e5ee3db6393b707122c7f6bb
David Terry <me@xwvvvvwx.com>
2020-09-29 16:19:33 +0200

    init

--- src/LockBenchmarks.t.sol
+++ src/LockBenchmarks.t.sol
@@ -2,20 +2,60 @@ pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
 
-import "./LockBenchmarks.sol";
+contract SingleStore {
+    uint nonce = 0;
+    modifier lock() {
+       uint _nonce = nonce;
+       nonce++;
+       _;
+       require(_nonce + 1 == nonce);
+    }
+
+    function f() lock public {}
+}
+
+contract DoubleStoreTFT {
+    bool unlocked = true;
+    modifier lock() {
+      unlocked = false;
+      _;
+      unlocked = true;
+    }
+
+    function f() lock public {}
+}
+
+contract DoubleStoreFTF {
+    bool locked = false;
+    modifier lock() {
+      locked = true;
+      _;
+      locked = false;
+    }
+
+    function f() lock public {}
+}
 
 contract LockBenchmarksTest is DSTest {
-    LockBenchmarks benchmarks;
+    SingleStore single;
+    DoubleStoreFTF doubleFTF;
+    DoubleStoreTFT doubleTFT;
 
     function setUp() public {
-        benchmarks = new LockBenchmarks();
+        single = new SingleStore();
+        doubleFTF = new DoubleStoreFTF();
+        doubleTFT = new DoubleStoreTFT();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_single() public {
+        single.f();
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_doubleFTF() public {
+        doubleFTF.f();
+    }
+
+    function test_doubleTFT() public {
+        doubleTFT.f();
     }
 }
